4f07639fe88f6eec2a11746e44844b0bf67ad83d
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2021-02-06 19:52:12 -0500

    master

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React from 'react';
 import '../App.css';
-import { Button } from "./Button";
+import { Button } from './Button';
 import './HeroSection.css';
+import video from '../assets/videos/video-1.mp4';
+
 
 function HeroSection() {
   return (
-    <div className="hero-container">
-      <video src="/src/videos/video-2.mp4" autoPlay loop muted />
+    <div className='hero-container'>
+      <video src={video} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className="hero-btns">
+      <div className='hero-btns'>
         <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
         >
-            GET STARTED
+          GET STARTED
         </Button>
         <Button
-          className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
         >
-            WATCH TRAILER <i className='far fa-play-circle' />
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
-        
       </div>
     </div>
   );
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "../../App.css";
-import HeroSection  from "../HeroSection";
-//asdfasdfasdf
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import HeroSection from '../HeroSection';
+
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
+      <Footer />
     </>
   );
 }
