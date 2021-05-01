a85ce967d8334cb5daabcd5686417ed8b9930c1b
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2021-05-01 15:06:47 -0500

    master

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='/react_web_responsive' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
